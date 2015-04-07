1427926ee811396526f99517f2c1fa097242874f
Gilmar Lira <gilmar.lira@me.com>
2015-04-07 15:32:11 -0700

    Starts repo for inertia

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+/* **********************************************
+     Begin interaction.js
+********************************************** */// INTERACTION
+// ////////////////////////////////////////////////////
+function tryout(e){console.log("works...");var t=document.getElementsByClassName("tryout_display");for(i=0;i<t.length;i++)t[i].textContent=e};
